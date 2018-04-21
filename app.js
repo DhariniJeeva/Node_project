a5a9831d0369bada6f45b2a88e0685bd784cbbf2
DJ <DJ@Dharinis-MacBook-Pro-3.local>
2018-04-21 12:49:20 -0400

    changed port

--- app.js
+++ app.js
@@ -112,7 +112,12 @@ app.post('/editPage' , user.editPage) ;
 //Middleware
 //app.listen('port')
 
-var port = process.env.PORT || 7777;
-app.listen(port, function() {
-    console.log("Listening on " + port);
+// var port = process.env.PORT || 7777;
+// app.listen(port, function() {
+//     console.log("Listening on " + port);
+// });
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
