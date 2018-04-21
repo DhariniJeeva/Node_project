d349f29b374c36bbd406b770a87a630054abef1f
DJ <DJ@Dharinis-MacBook-Pro-3.local>
2018-04-21 13:29:51 -0400

    fixed email settings

--- routes/user.js
+++ routes/user.js
@@ -251,10 +251,49 @@
                         var success = "Succesfully! data added";
                         });
 
-              res.render('contact');
+              //res.render('contact');
               //res.render('editPage' , message);
 
       }
+                    const output = `
+                    <p>you have a new contact request</p>
+                    <h3>Contact Form</h3>
+                    <ul>
+                      <li>Name :` + req.body + `</li>
+                    </ul>`
+          let transporter = nodemailer.createTransport({
+              host: 'smtp.gmail.com'
+              , port: 587
+              , secure: false, // true for 465, false for other ports
+              auth: {
+                  user: 'ritinternationalhelper@gmail.com', // generated ethereal user
+                  pass: 'boombox123' // generated ethereal password
+              }
+              , tls: {
+                  rejectUnauthorized: false
+              }
+          });
+          // setup email data with unicode symbols
+          let mailOptions = {
+              from: req.body.email, // sender address
+              to: "ritinternationalhelper@gmail.com", // list of receivers
+              subject: 'Node Contact Request', // Subject line
+              text: 'Hello world?', // plain text body
+              html: output // html body
+          };
+          // send mail with defined transport object
+          transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                  return console.log(error);
+              }
+              console.log('Message sent: %s', info.messageId);
+              console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+              res.render('contact', {
+                  message: 'Email has been sent!'
+              });
+          });
+          
+          res.render('contact');
 
   //        var tableToCsv = require('node-table-to-csv');
   //
@@ -279,45 +318,7 @@
               //res.render('uploadfiles.ejs');
   //        });
 
-  //        const output = `
-  //                  <p>you have a new contact request</p>
-  //                  <h3>Contact Form</h3>
-  //                  <ul>
-  //                    <li>Name :` + req.body.name + `</li>
-  //                    <li>Email :` + req.body.email + `</li>
-  //                    <li>Message :` + req.body.message + `</li>
-  //                  </ul>`
-  //        let transporter = nodemailer.createTransport({
-  //            host: 'smtp.gmail.com'
-  //            , port: 587
-  //            , secure: false, // true for 465, false for other ports
-  //            auth: {
-  //                user: 'ritinternationalhelper@gmail.com', // generated ethereal user
-  //                pass: 'boombox123' // generated ethereal password
-  //            }
-  //            , tls: {
-  //                rejectUnauthorized: false
-  //            }
-  //        });
-  //        // setup email data with unicode symbols
-  //        let mailOptions = {
-  //            from: req.body.email, // sender address
-  //            to: "ritinternationalhelper@gmail.com", // list of receivers
-  //            subject: 'Node Contact Request', // Subject line
-  //            text: 'Hello world?', // plain text body
-  //            html: output // html body
-  //        };
-  //        // send mail with defined transport object
-  //        transporter.sendMail(mailOptions, (error, info) => {
-  //            if (error) {
-  //                return console.log(error);
-  //            }
-  //            console.log('Message sent: %s', info.messageId);
-  //            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  //            res.render('contact', {
-  //                message: 'Email has been sent!'
-  //            });
-  //        });
+
       }
       else {
           res.render('contact');
