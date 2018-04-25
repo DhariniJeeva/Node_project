7c28990402cd8928c97dc71f3a2eb1b984af7dbd
DJ <DJ@Dharinis-MacBook-Pro-3.local>
2018-04-25 19:16:54 -0400

    fixed some settings here and there

--- app.js
+++ app.js
@@ -118,6 +118,11 @@ app.post('/editPage' , user.editPage) ;
 // });
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
 });
